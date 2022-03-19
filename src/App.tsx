1f681a3fe4ea79f9bcaeea5c8d03086e2112cb07
Lincoln Ruteski <lruteski@gmail.com>
2022-03-19 17:44:09 -0300

    instalado styled-components, remanejado pacotes para o devdependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+   color: #8257e6;
+   font-size: 64px;
+`;
 
 export function App() {
-   return  <div className="App" />;
+   return (
+      <div className="App">
+         <Title>Hello world</Title>
+      </div>
+   );
 }
